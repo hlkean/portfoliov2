83160d5cf9785282c17fb67ce78a0fa6ba0edb3f
hlkean <hlkean@gmail.com>
2021-10-27 22:21:27 -0400

    clean up copy

--- src/routes/About.js
+++ src/routes/About.js
@@ -15,19 +15,19 @@ class About extends React.Component {
           <Paraletters id="about-header" tag="h1" classes="-p-top-25" letters="About" limitX="50" limitY="40"/>
           <div className="-max-w-950 -p-y-25 -m-0-auto">
               <div className="-p-x-25 -flex -justify-center -space-between -align-items-center -txt-img-50-50">
-                <h2 className="-m-top-0">Hey there, tiger</h2>
+                <h2 className="-m-top-0">Hey there,</h2>
                 <img src={Tiger} alt=""/>
               </div>
-              <p>I’m a full-stack developer with a passion for coming up with new ideas and learning new things. Combing an education in psychology with a master’s degree in Interactive media, I love to think up with new projects or solutions to problems backed by qualitative and quantitative data.</p>
+              <p>I’m a former aspiring therapist turned software engineer with a passion for coming up with new ideas and learning new things. Combing an education in psychology with a master’s degree in Interactive media, I love to think up new projects or solutions to problems backed by qualitative and quantitative data. I thrive in collaborative project environments where I can learn from, work alongside, and mentor those around me.</p>
               <p>Though my job title has primarily been as an engineer, I have contributed insights and ideas to international digital campaigns for enterprise level clients, helped win new business, won startup competitions, and aided in developing a business growing measurement culture in a startup. Below you'll find an ever expanding list of tools that I've used along the way to help create and build some amazing projects:</p>
               <ul>
                 <li>Front-end: HTML, CSS/SCSS, Javascript/jQuery/ES6</li>
-                <li>Front-end Frameworks: Vue, Ember, React</li>
-                <li>Backend: PHP/Laravel/CodeIgniter, MySQL</li>
-                <li>SEO: SEMRush, Google Search Console, Lighthouse</li>
-                <li>CMS: Grav, Hippo</li>
-                <li>Design/Creative: Adobe Creative Suite (primarily Photoshop, Illustrator, and AfterEffects), Sketch, Figma</li>
-                <li>Other: Git, Gulp, Jira</li>
+                <li>Front-end Frameworks: React, React Native, Vue, Ember</li>
+                <li>Backend: PHP/Laravel/CodeIgniter, MySQL, Node.js, GraphQL</li>
+                <li>SEO/Metrics: SEMRush, Google Search Console, Lighthouse, Google Analytics, Firebase Performance Monitoring, Firebase Crashlytics</li>
+                <li>CMS: Grav, Hippo, Wordpress</li>
+                <li>Design/Creative: Adobe Creative Cloud (primarily Photoshop, Illustrator, and AfterEffects), Sketch</li>
+                <li>Other: Git, Gulp, Atlassian Productivity Suite</li>
               </ul>
               <p>I’m always looking for new experiences. If you’d like to work together, or want to learn more about my previous work, please don’t hesitate to reach out!</p>
           </div>
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -13,8 +13,7 @@ class Contact extends React.Component {
           </Helmet>
           <Paraletters id="contact-header" tag="h1" classes="-p-top-25" letters="Contact" limitX="50" limitY="40"/>
           <div class="-max-w-950 -p-y-25 -m-0-auto -flex -flex-1 -justify-center -col">
-              <p>I grew up in New Jersey, got a Master's in North Carolina, cut my teeth in Texas, and now reside in New York where I work as a software engineer.</p>
-              <p>I'm always looking for new opportunities to work on exciting new projects. Feel free to take a copy of <a href={Resume} target="_blank">my résumé</a> and <a href="mailto:hlkean@gmail.com">reach out</a> if you'd like to learn more about my past projects or work together on a future one.</p>
+              <p>I'm always looking for new opportunities to work on exciting projects. Feel free to take a copy of <a href={Resume} target="_blank">my résumé</a> and <a href="mailto:hlkean@gmail.com">reach out</a> if you'd like to learn more about my past projects or work together on a future one.</p>
               <p>+1 (908) 285-8030 | <a href="mailto:hlkean@gmail.com">hlkean@gmail.com</a></p>
           </div>
               
